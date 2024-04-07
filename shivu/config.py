eca161c2bc9135ca3ccd0844decb9fa70f7fe30c
Imsleepyyy <86151641+Imsleepyyy@users.noreply.github.com>
2024-04-07 23:35:48 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "1853342928"
     sudo_users = "1853342928", "6327652067"
     GROUP_ID = -1001945562571
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    TOKEN = "6657141119:AAE9He0fZ_EICUt6NTDZUavjoYIwrODfnfg"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
+    SUPPORT_CHAT = "lavda_lasan"
+    UPDATE_CHAT = "lavda_lasan"
+    BOT_USERNAME = "lavda_lasan"
     CHARA_CHANNEL_ID = "-1001945562571"
     api_id = 24726690
     api_hash = "1452fafefb8b331e1118a7363d364e16"
